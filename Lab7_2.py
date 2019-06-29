f028551bbe6f3a8964747ff43ca231a912bc0952
Pirognoe <ipirognoe@gmail.com@>
2019-06-29 21:19:15 +0300

    2nd computer

--- Lab7_2.py
+++ Lab7_2.py
@@ -1,6 +1,6 @@
 import math
 
-#Вынес настройку курса вне класса - для пробы пера
+#Declare first configuration for testing
 conf = {
         'exam_max': 30,
         'lab_max': 7,
@@ -9,7 +9,6 @@ conf = {
     }
 
 class Student(object):
-    name = 'Lox'
 
     def __init__(self, name, conf):
         self.name = name
@@ -20,53 +19,70 @@ class Student(object):
             self.labs.append(0)
         # Create variable to store exam result
         self.exam = 0
-    #def error_check(self)
 
-    def make_lab(self,m,n):
-        if m > conf['lab_max']:
+
+    def make_lab(self,m,n=0):
+        if m > self.conf['lab_max']:
+            print(m, self.conf['lab_max'], m > self.conf['lab_max'])
+            return self
+        elif n > self.conf['lab_num']:
             pass
+            return self
+        else:
+            print('zhopa', n, m)
+            self.labs[n] = m
+            print(self.labs[n])
+        return self
 
-    #def make_exam(m)
+    def make_exam(self, m):
+        if m > self.conf['exam_max']:
+            print(m, self.conf['exam_max'], m > self.conf['exam_max'])
+            return self
+        return self
 
-    #def is_certified()
+    def is_certified(self):
+        pass
+        return self
 
 
 
 conf1 = {'exam_max': 20, 'lab_max': 40, 'lab_num': 12, 'k': 0.75}
 o1 = Student('Oleg', conf1)
-print
+print('============================Conf_1=====================')
 o1.is_certified(), '== (0, False)'
 o2 = Student('Oleg', conf1)
+o2.make_lab(35.2)
+print(o2.labs)
 o2.make_lab(60).make_lab(35.2)
-print
+print('============================Conf_2=====================')
 o2.is_certified(), '== (75.2, True)'
 o3 = Student('Oleg', conf1)
 o3.make_lab(10).make_lab(10).make_exam(15)
-print
+print('============================Conf_3=====================')
 o3.is_certified(), '== (35, False)'
 o3.make_lab(20, 1).make_lab(20, 0),
-print
+print('============================Conf_4=====================')
 o3.is_certified(), '== (55, False)'
 o3.make_lab(50, 2)
 o3.is_certified()
 o3.make_lab(40, 1)
-print
+print('============================Conf_5=====================')
 o3.is_certified(), '== (75, True)'
 conf2 = {'exam_max': 52, 'lab_max': 8, 'lab_num': 6, 'k': 0.5}
 o4 = Student('Oleg', conf2)
 o4.make_exam(100)
-print
+print('============================Conf_6=====================')
 o4.is_certified(), '== (52, True)'
 o5 = Student('Oleg', conf2)
 o5.make_lab(40).make_lab(7, 5).make_lab(1).make_lab(7).make_lab(7).make_lab(7).make_lab(7, 1)
-print
+print('============================Conf_7=====================')
 o5.is_certified(), '== (43, False)'
 o5.make_lab(7)
-print
+print('============================Conf_8=====================')
 o5.is_certified(), '== (43, False)'
 o5.make_exam(7)
 conf3 = {'exam_max': 10, 'lab_max': 1, 'lab_num': 90, 'k': 0.5, }
 o6 = Student('Oleg', conf3)
 for i in range(51): o6.make_lab(1)
-print
+print('============================Conf_9=====================')
 o6.is_certified(), '== (51, True)'
