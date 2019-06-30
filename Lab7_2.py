972313aa6c6dc8d4e218b7f9ccb35245a2f8d097
Ivan <ipirognoe@gmail.com>
2019-06-30 18:29:05 +0300

    Final version 18:29 on 30/06/2019

--- Lab7_2.py
+++ Lab7_2.py
@@ -1,5 +1,5 @@
 # Declare initial configuration for testing
-conf = {
+conf0 = {
     'exam_max': 30,
     'lab_max': 7,
     'lab_num': 10,
@@ -22,15 +22,17 @@ class Student(object):
         self.enough_score = self.conf['k'] * self.maximum_score
 
 
+
     def make_lab(self, m, n=0):
         if m > self.conf['lab_max']:
             return self
         elif n > self.conf['lab_num']:
-            pass
             return self
         elif n != 0:
-            self.labs[n - 1] = m
+            self.labs[n] = m
         elif n == 0:
+            if self.labs[0] < m:
+                self.labs[0] = m
             for i in range(len(self.labs)):
                 if self.labs[i] == 0:
                     self.labs[i] = m
@@ -47,40 +49,36 @@ class Student(object):
 
     def is_certified(self):
         self.numeric_result = self.exam + sum(self.labs)
-        self.logic_result = self.numeric_result > self.enough_score
-        return self.numeric_result, self.logic_result
+        self.logic_result = self.numeric_result >= self.enough_score
+        return (self.numeric_result, self.logic_result)
 
 
-conf1 = {'exam_max': 20, 'lab_max': 40, 'lab_num': 12, 'k': 0.75}
+conf1 = {'exam_max': 20,'lab_max': 40,'lab_num': 2,'k': 0.75}
 o1 = Student('Oleg', conf1)
-print(o1.is_certified(), '== (0, False)')
+print(o1.labs, o1.exam, o1.is_certified(), '(0, False)')
 o2 = Student('Oleg', conf1)
-o2.make_lab(35.2)
 o2.make_lab(60).make_lab(35.2)
-print(o2.labs)
-print('============================Conf_2=====================')
-o2.is_certified(), '== (75.2, True)'
+print (o2.labs, o2.exam, o2.is_certified(), '(75.2, True)')
 o3 = Student('Oleg', conf1)
 o3.make_lab(10).make_lab(10).make_exam(15)
-print(o3.labs, o3.is_certified(), '== (35, False)')
-o3.make_lab(20, 1).make_lab(20, 0),
-print(o3.labs, o3.is_certified(), '== (55, False)')
-o3.make_lab(50, 2)
-o3.is_certified()
-o3.make_lab(40, 1)
-print('============================Conf_5=====================')
-print(o3.labs, o3.is_certified(), '== (75, True)')
-conf2 = {'exam_max': 52, 'lab_max': 8, 'lab_num': 6, 'k': 0.5}
+print (o3.labs, o3.exam,o3.is_certified(), '(35, False)')
+o3.make_lab(20,1).make_lab(20,0)
+print (o3.labs, o3.exam, o3.is_certified(), '(55, False)')
+o3.make_lab(50,2)
+print (o3.labs, o3.exam, o3.is_certified(), '(55, False)')
+o3.make_lab(40,1)
+print (o3.labs, o3.exam, o3.is_certified(), '(75, True)')
+conf2 = {'exam_max': 52,'lab_max': 8,'lab_num': 6,'k': 0.5}
 o4 = Student('Oleg', conf2)
 o4.make_exam(100)
-print(o4.labs, o4.is_certified(), '== (52, True)')
+print (o4.labs, o4.exam, o4.is_certified(), '(52, True)')
 o5 = Student('Oleg', conf2)
-o5.make_lab(40).make_lab(7, 5).make_lab(1).make_lab(7).make_lab(7).make_lab(7).make_lab(7, 1)
-print(o5.is_certified(), '== (43, False)')
+o5.make_lab(40).make_lab(7,5).make_lab(1).make_lab(7).make_lab(7).make_lab(7).make_lab(7,1)
+print (o5.labs, o5.exam, o5.is_certified(), '(43, False)')
 o5.make_lab(7)
-print(o5.labs, o5.is_certified(), '== (43, False)')
+print (o5.labs, o5.exam, o5.is_certified(), '(43, False)')
 o5.make_exam(7)
-conf3 = {'exam_max': 10, 'lab_max': 1, 'lab_num': 90, 'k': 0.5, }
+conf3 = {'exam_max': 10,'lab_max': 1,'lab_num': 90,'k': 0.5,}
 o6 = Student('Oleg', conf3)
 for i in range(51): o6.make_lab(1)
-print(o6.is_certified(), '== (51, True)')
+print (o6.labs, o6.exam, o6.is_certified(), '(51, True)')
